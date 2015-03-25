4d67c9a1a4c137112c8e32d680df60c5150dc6e2
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-03-25 14:05:28 -0400

    modified shrinkpdf

--- shrinkpdf.sh
+++ shrinkpdf.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-gs	-q -dNOPAUSE -dBATCH -dSAFER \
-	-sDEVICE=pdfwrite \
-	-dCompatibilityLevel=1.3 \
-	-dPDFSETTINGS=/screen \
-	-dEmbedAllFonts=true \
-	-dSubsetFonts=true \
-	-dColorImageDownsampleType=/Bicubic \
-	-dColorImageResolution=72 \
-	-dGrayImageDownsampleType=/Bicubic \
-	-dGrayImageResolution=72 \
-	-dMonoImageDownsampleType=/Bicubic \
-	-dMonoImageResolution=72 \
-	-sOutputFile=out.pdf \
-	 $1
+# gs	-q -dNOPAUSE -dBATCH -dSAFER \
+# 	-sDEVICE=pdfwrite \
+# 	-dCompatibilityLevel=1.3 \
+# 	-dPDFSETTINGS=/screen \
+# 	-dEmbedAllFonts=true \
+# 	-dSubsetFonts=true \
+# 	-dColorImageDownsampleType=/Bicubic \
+# 	-dColorImageResolution=72 \
+# 	-dGrayImageDownsampleType=/Bicubic \
+# 	-dGrayImageResolution=72 \
+# 	-dMonoImageDownsampleType=/Bicubic \
+# 	-dMonoImageResolution=72 \
+# 	-sOutputFile=out.pdf \
+# 	 $1
+
+gs -sDEVICE=pdfwrite \
+    -dCompatibilityLevel=1.4 \
+    -dPDFSETTINGS=/screen \
+    -dNOPAUSE \
+    -dQUIET \
+    -dBATCH \
+    -sOutputFile=lowres.pdf \
+    $1
